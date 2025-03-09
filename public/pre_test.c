e588b6e47653d1a5fdbbe15b72dde88a8b3a6087
Stockdr <102687197+stockdr@users.noreply.github.com>
2025-03-09 11:24:54 +0000

    ㅇㄹㅇ

--- public/pre_test.c
+++ public/pre_test.c
@@ -8,22 +8,17 @@ int main()
     int numOfMatrix = 0;
     printf("행렬의 크기 n 값을 입력하세요 : ");
     scanf("%d", &numOfMatrix);
-    printf("%d x %d 행렬\n", numOfMatrix, numOfMatrix);
+    printf("%d x %d 행렬", numOfMatrix, numOfMatrix);
 
     srand(time(NULL));                                            // rand의 seed값 변경
     int* mat = (int*)malloc(sizeof(int)*numOfMatrix*numOfMatrix); // n*n 배열 생성
-    int* sum = (int*)malloc(sizeof(int)*numOfMatrix*2+2);         // 행,열,대각선 합 저장용
-
-    for (int i = 0; i < numOfMatrix*numOfMatrix; ++i) {
-        mat[i] = rand() % 1000;
-
-        if (i % numOfMatrix == 0) {
-            printf("\n");
-            printf("%7d ", mat[i]);
-        }
-        else
-            printf("%7d ", mat[i]);
-    }
+    int sumLength = numOfMatrix*2 + 2;
+    int* sum = (int*)malloc(sizeof(int)*sumLength);         // 행,열,대각선 합 저장용
+    for (int i = 0; i < sumLength; ++i)
+        sum[i] = 0;
+    
+    for (int i = 0; i < numOfMatrix*numOfMatrix; ++i)   // 랜덤값 넣기
+        mat[i] = rand() % 10;
 
     // sum에 값 넣기
     int rowSum = 0, colSum = 0;
@@ -39,13 +34,35 @@ int main()
         rowSum = 0;
         colSum = 0;
     }
-    for (int i = numOfMatrix*numOfMatrix - 1; i >= 0; i -= numOfMatrix + 1)
+    for (int i = numOfMatrix*numOfMatrix - 1; i >= 0;)
     {
-        sum[numOfMatrix*2] += mat[i];
+        sum[sumLength - 2] += mat[i];
+        i -= numOfMatrix+1;
     }
-    for (int i = numOfMatrix - 1; i < numOfMatrix*(numOfMatrix-1); i *= (numOfMatrix-1))
+    for (int i = numOfMatrix - 1; i <= numOfMatrix*(numOfMatrix-1); i += (numOfMatrix-1))
     {
-        
+        sum[sumLength - 1] += mat[i];
+    }
+
+    // 최대값 찾기
+    int maxNumOfLine = 0;
+    int lineNum = 0;
+    char c;
+    for(int i = sumLength - 1; i >= 0; --i)
+    {
+        if (maxNumOfLine < sum[i]) {
+            if (i >= 0 && i < numOfMatrix)
+                c = 'r';
+            else if (i >= numOfMatrix && i < numOfMatrix*2)
+                c = 'c';
+            else if (i == sumLength - 2)
+                c = 'd';
+            else if (i == sumLength - 1)
+                c = 'u';
+            
+            maxNumOfLine = sum[i];
+            lineNum = i + 1;
+        }
     }
 
     // 행렬 크기 10보다 작으면 행렬값 출력
@@ -58,7 +75,26 @@ int main()
             else
                 printf("%7d ", mat[i]);
         }
+        printf("\n");
     }
-    
-    
+
+    printf("최대값은 ");
+    switch(c)
+    {
+        case 'r': 
+            printf("%d행", lineNum);
+            break;
+        case 'c':
+            printf("%d열", lineNum);
+            break;
+        case 'u':
+            printf("우상좌하 대각선");
+            break;
+        case 'd':
+            printf("좌상우하 대각선");
+            break;
+        default:
+            break;
+    }
+    printf("의 합 %d\n", maxNumOfLine);
 }
